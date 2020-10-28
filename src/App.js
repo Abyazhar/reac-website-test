a3fdc5163e6fca5ef0dcfbbd70c328ffd76b6b7b
Aby <67815903+Abyazhar@users.noreply.github.com>
2020-10-28 16:05:23 +0800

    add videos

--- src/App.js
+++ src/App.js
@@ -5,17 +5,19 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <video src='videos/videos-3.mp4' autoPlay loop/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello React! Im back
         </a>
       </header>
     </div>
